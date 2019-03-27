c4c6bb9dff39b0c1121fd755620db796c23460e6
laucherish <laucherish@gmail.com>
2019-03-27 16:58:11 +0800

    完成线程切换功能

--- app/src/main/java/com/laucherish/pureeventbus/lib/EventBus.java
+++ app/src/main/java/com/laucherish/pureeventbus/lib/EventBus.java
@@ -1,5 +1,8 @@
 package com.laucherish.pureeventbus.lib;
 
+import android.os.Handler;
+import android.os.Looper;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -7,6 +10,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * EventBus核心类
@@ -17,10 +22,14 @@ import java.util.Set;
 public class EventBus {
 
     private static volatile EventBus instance;
-    private Map<Object, List<SubscribeMethod>> cacheMap;
+    private Map<Object, List<SubscribeMethod>> cacheMap; // 存储所有订阅方法
+    private Handler handler; // handler用于将子线程任务切换到主线程执行
+    private ExecutorService executorService; // 线程池用于在子线程中执行任务
 
     private EventBus() {
         cacheMap = new HashMap<>();
+        handler = new Handler(Looper.getMainLooper());
+        executorService = Executors.newCachedThreadPool();
     }
 
     /**
@@ -73,17 +82,47 @@ public class EventBus {
      * 发布者发布事件
      * @param type 事件参数
      */
-    public void post(Object type) {
+    public void post(final Object type) {
         Set<Object> set = cacheMap.keySet();
         // 通过foreach遍历cacheMap
-        for (Object obj : set) {
+        for (final Object obj : set) {
             // 取出每一个订阅者的所有SubscribeMethod
             List<SubscribeMethod> list = cacheMap.get(obj);
             if (list != null) {
-                for (SubscribeMethod subscribeMethod : list) {
+                for (final SubscribeMethod subscribeMethod : list) {
                     // 判断订阅方法的参数类和事件参数类型大致相同
                     if (subscribeMethod.getType().isAssignableFrom(type.getClass())) {
-                        invoke(obj, subscribeMethod, type);
+                        // 根据threadMode判断执行线程
+                        switch (subscribeMethod.getThreadMode()) {
+                            case MAIN:
+                                if (Looper.myLooper() == Looper.getMainLooper()) {
+                                    // 主线程 发送到 主线程
+                                    invoke(obj, subscribeMethod, type);
+                                } else {
+                                    // 子线程 发送到 主线程
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            invoke(obj, subscribeMethod, type);
+                                        }
+                                    });
+                                }
+                                break;
+                            case BACKGROUND:
+                                if (Looper.myLooper() == Looper.getMainLooper()) {
+                                    // 主线程 发送到 子线程
+                                    executorService.execute(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            invoke(obj, subscribeMethod, type);
+                                        }
+                                    });
+                                } else {
+                                    // 子线程 发送到 子线程
+                                    invoke(obj, subscribeMethod, type);
+                                }
+                                break;
+                        }
                     }
                 }
             }
